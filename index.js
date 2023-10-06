832ebd6f48024746b93c918e83c4a6f98dd32f08
Hala Qitouqa <h.qitouqa@gmail.com>
2023-10-06 13:51:50 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+function saturdayFun(action="roller-skate"){
+     console.log(`This Saturday, I want to ${action}!`)
+}
+   
+
+saturdayFun()
+
+function mondayWork(activity){
+    if(activity==undefined){
+    console.log("This Monday, I will go to the office.")}
+    else{
+        console.log(`This Monday, I will ${activity}.`)
+    }
+}
+mondayWork();
+
+//let result = wrapAdjective("||")
+//let emphatic = result("a dedicated programmer")
+function wrapAdjective(initial, adj){
+    if(initial=='*'){
+        console.log(`You are *${adj}*!`)
+    }
+    if(initial=='||'){
+        console.log(`You are ||${adj}||!`)
+    }
+}
+
+wrapAdjective('||',"a dedicated programmer")
+
